5288c9ff7d9e4c3cba80bd931a7664a734e749e1
clmirabent <leomiracar@gmail.com>
2023-11-29 10:23:23 +0100

    schema-user

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {connectDB} =  require("./mongo/connection");
+const { connectDB } = require("./mongo/connection");
 const cors = require('cors');
 const app = express();
 app.use(cors());
---
+++ backend/src/mongo/schemas/user.js
@@ -0,0 +1,13 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema({
+    name: { type: String, required: true },
+    lastName: { type: String, required: false },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    location: { type: String, required: true },
+});
+
+const User = model('user', userSchema);
+
+module.exports = User;
