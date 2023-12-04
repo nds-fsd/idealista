89f919a6f8c129fd12893e5504cc2645822a655e
clmirabent <leomiracar@gmail.com>
2023-12-04 13:49:54 +0100

    last-controller fix

--- backend/src/controllers/users.js
+++ backend/src/controllers/users.js
@@ -2,16 +2,18 @@ const express = require('express');
 const User = require("../mongo/schemas/users");
 
 const getAll = async (req, res) => {
-
     try {
         const queryStrings = req.query || {};
         const allUsers = await User.find(queryStrings);
-        res.status(200).json(allUsers);
+        if (allUsers) {
+            res.status(200).json(allUsers);
+        } else {
+            res.status(404).send()
+        }
     } catch (error) {
         console.log("Error in users.js getAll():", error)
-        response.status(500).send(error.message)
+        res.status(500).send(error.message)
     }
-
 };
 
 const getById = async (req, res) => {
@@ -20,27 +22,29 @@ const getById = async (req, res) => {
         if (user) {
             res.status(200).json(user);
         } else {
-            res.status(404)
+            res.status(404).send()
         }
     } catch (error) {
         console.log("Error in users.js getById():", error)
-        response.status(500).send(error.message)
+        res.status(500).send(error.message)
     }
-
 };
 
 const update = async (req, res) => {
     try {
-        const allUsers = await User.findByIdAndUpdate(req.params.id, req.body, {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, {
             new: true,
             upsert: false,
-        }); res.status(200).json(allUsers);
+        });
+        if (user) {
+            res.status(200).json(user);
+        } else {
+            res.status(404).send();
+        }
     } catch (error) {
         console.log("Error in users.js update():", error)
-        response.status(500).send(error.message)
+        res.status(500).send(error.message)
     }
-
-
 };
 
 const remove = async (req, res) => {
@@ -49,13 +53,12 @@ const remove = async (req, res) => {
         if (user) {
             res.status(201).json(user);
         } else {
-            res.status(400).send();
+            res.status(404).send();
         }
     } catch (error) {
         console.log("Error in users.js remove():", error)
-        response.status(500).send(error.message)
+        res.status(500).send(error.message)
     }
-
 };
 
 const create = async (req, res) => {
@@ -70,8 +73,6 @@ const create = async (req, res) => {
         console.log("Error in users.js create():", error);
         res.status(500).send(error.message);
     }
-
-
 }
 
 module.exports = {
