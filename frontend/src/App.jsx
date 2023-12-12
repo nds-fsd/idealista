7d223edf05dbd4d01db2b287f5d4a17fa128ff32
Xavier Lopez <xlopezmin@gmail.com>
2023-12-12 11:28:32 +0100

    fix(browserrouter): revolved comment PR

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter } from 'react-router-dom'
 import {Routes, Route, Link} from "react-router-dom";
 import RealEstateList from "./components/realestates/realestateslist/RealEstatesList";
 import RealEstateListElement from "./components/realestates/realestatelistelement/RealEstateListElement";
@@ -7,23 +8,23 @@ import style from "./App.module.css";
 
 function App() {
     return (
-      <>
+      <BrowserRouter>
         Your App here
         <div className={style.app}>
           <nav className={style.nav}>
-            <lu>
+            <ul>
               <li><Link to="/realestates">Buscador inmuebles</Link></li>
               <li><Link to="/realestates/1">Inmueble</Link></li>
-            </lu>
+            </ul>
           </nav>
           <div className={style.components}>
-            <Routes>
-              <Route path="/realestates" element={<RealEstateList/>}></Route>
-              <Route path="/realestates/:id" element={<RealEstateListElement/>}></Route>
-            </Routes>
+              <Routes>
+                <Route path="/realestates" element={<RealEstateList/>}></Route>
+                <Route path="/realestates/:id" element={<RealEstateListElement/>}></Route>
+              </Routes>
           </div>
         </div>
-      </>
+      </BrowserRouter>
   )
 }
 
