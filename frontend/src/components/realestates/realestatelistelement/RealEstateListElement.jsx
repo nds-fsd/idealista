27fb6c203a16a10959318347bae5e5049dbe14da
Xavier Lopez <xlopezmin@gmail.com>
2023-12-15 15:14:17 +0100

    feac(realestatelistelement): update card and styles

--- frontend/src/components/realestates/realestatelistelement/RealEstateListElement.jsx
+++ frontend/src/components/realestates/realestatelistelement/RealEstateListElement.jsx
@@ -1,15 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import styles from "./RealEstateListElement.module.css"
+import house_image from "../../../assets/pexels-aaron-cook-19277901 1.png"
+
 function RealEstateListElement({ realEstate }) {
 
     return (
-        <Link to={`/realestates/${realEstate._id}`}>
-            <div >
-                <p>{realEstate.shortDescription}</p>
-                <p>{realEstate.price}</p>
+        <div className={styles.card}>
+            <div>
+                <Link to={`/realestates/${realEstate._id}`}>
+                    <img className={styles.image} src={house_image} alt="Imagen inmueble"/>
+                </Link>
             </div>
-        </Link>
+            <div>
+                <Link className={styles.link} to={`/realestates/${realEstate._id}`}>
+                    <h2>{realEstate.shortDescription}</h2>
+                </Link>
+                <div>{realEstate.properties}</div>
+                <div>{realEstate.state}</div>
+                <div>{realEstate.price}</div>
+            </div>            
+        </div>
     )
 }
 
