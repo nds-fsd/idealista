95dd63f0c1c3af234afcf017cc8af88adf775a8f
Xavier Lopez <xlopezmin@gmail.com>
2023-12-22 15:40:34 +0100

    feac(googlemapsmarker): init google maps marked

--- frontend/src/components/googleMaps/map/GoogleMaps.jsx
+++ frontend/src/components/googleMaps/map/GoogleMaps.jsx
@@ -1,20 +1,29 @@
 import React from "react";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
-function GoogleMaps() {
+function GoogleMaps(props) {
     const ref = useRef(null);
+    const [map, setMap] = useState();
 
     useEffect(() => {
-        if (ref.current) {
-            new window.google.maps.Map(ref.current, {
-                center: { lat: 48.8566, lng: 2.3522 },
-                zoom: 13,
-            });
+        if (ref.current && !map) {
+            setMap(
+                  new window.google.maps.Map(ref.current, {
+                    center: { lat: 48.8566, lng: 2.3522 },
+                    zoom: 13,}
+                ));
         }
-    }, [ref]);
+    }, [ref, map]);
 
     return (
-        <div ref = {ref} style={{ margin: "auto", width: "1140px", height: "600px" }} />
+        <>
+            <div ref={ref} style={{ margin: "auto", width: "1140px", height: "600px" }} />
+            {                
+                props.children.map((child) => {
+                    return React.cloneElement(child, {map});
+            }
+            )}
+        </>
     )
 }
 
--- frontend/src/pages/realEstates/realEstateListMap/RealEstateListMap.jsx
+++ frontend/src/pages/realEstates/realEstateListMap/RealEstateListMap.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import GoogleMapsReactWrapper from "../../../components/googleMaps/reactWrapper/GoogleMapsReactWrapper";
 import GoogleMaps from "../../../components/googleMaps/map/GoogleMaps";
+import GoogleMapsMarker from "../../../components/googleMaps/marker/GoogleMapsMarker";
 
 function RealEstateListMap(){
   
     return (
         <GoogleMapsReactWrapper>
-            <GoogleMaps />
+            <GoogleMaps>
+                <GoogleMapsMarker key="re1" position={ {lat: 48.8565, lng: 2.3521} } />
+                <GoogleMapsMarker key="re2" position={ {lat: 48.8604, lng: 2.3520} } />
+                <GoogleMapsMarker key="re3" position={ {lat: 48.8704, lng: 2.3020} } />
+            </GoogleMaps>
         </GoogleMapsReactWrapper>
     )
   }
