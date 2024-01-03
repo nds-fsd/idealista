12be50987c46e722bda2db3d6e2ca5055a5f74f3
filip.galetic@gmail.com <filip.galetic@gmail.com>
2024-01-03 20:03:51 +0100

    Refactor real estate form and API, add basic css for the form, add submission message and helpful links

--- frontend/src/pages/realEstates/realEstateForm/realEstateForm.jsx
+++ frontend/src/pages/realEstates/realEstateForm/realEstateForm.jsx
@@ -1,26 +1,35 @@
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
-import axios from 'axios';
-
+import { CreateRealEstate } from '../../../utils/apis/realEstateApi.js';
+import styles from './realEstateForm.module.css';
 
 const RealEstateForm = () => {
   const { register, handleSubmit, formState: { errors } } = useForm();
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
-  const onSubmit = async data => {
-    try {
-      const response = await axios.post('/realestates', data);
-      console.log(response.data);
-    } catch (error) {
-      console.error('Error creating real estate', error);
-    }
+  const onSubmit = async (data) => {
+    const response = await CreateRealEstate(data);
+    setIsSubmitted(true);
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <div>
+      {isSubmitted ? (
+        <div className={styles.container}>
+          <p className={styles.successMessage}>¡Tu anuncio ha sido publicado exitosamente!</p>
+          <a href="/" className={styles.link}>Volver a la página de inicio</a>
+          <a href="/create-ad" className={styles.link}>Crear otro anuncio</a>
+        </div>
+      ) : (
+        <form onSubmit={handleSubmit(onSubmit)} className={styles.container}>
+          <h1 className={styles.title}>Llena este formulario y publica tu anuncio en un instante</h1>
 
-      <input {...register("shortDescription", { required: true })} placeholder="Descripción corta" />
-      {errors.shortDescription && <p>This field is required</p>}
+          <label htmlFor="shortDescription" className={styles.label}>Descripción corta</label>
+      <input {...register("shortDescription", { required: true })} className={styles.input} />
+      {errors.shortDescription && <p className={styles.error}>This field is required</p>}
 
-      <select {...register("realEstateType", { required: true })}>
+      <label htmlFor="realEstateType" className={styles.label}>Tipo de inmueble</label>
+      <select {...register("realEstateType", { required: true })} className={styles.select}>
         <option value="Vivienda">Viviendas</option>
         <option value="Promocion">Promoción</option>
         <option value="Oficinas">Oficinas</option>
@@ -30,31 +39,39 @@ const RealEstateForm = () => {
         <option value="Trastero">Trastero</option>
         <option value="Edificio">Edificio</option>
       </select>
-      {errors.realEstateType && <p>This field is required</p>}
+      {errors.realEstateType && <p className={styles.error}>This field is required</p>}
 
-      <input {...register("realEstateSubtype", { required: true })} placeholder="Subtipo de inmueble" />
-      {errors.realEstateSubtype && <p>This field is required</p>}
+      <label htmlFor="realEstateSubtype" className={styles.label}>Subtipo de inmueble</label>
+      <input {...register("realEstateSubtype", { required: true })} className={styles.input} />
+      {errors.realEstateSubtype && <p className={styles.error}>This field is required</p>}
 
-      <input type="number" {...register("metersBuilt", { required: true })} placeholder="Metros cuadrados (m²)" />
-      {errors.squareMeters && <p>This field is required</p>}
+      <label htmlFor="metersBuilt" className={styles.label}>Metros cuadrados (m²)</label>
+      <input type="number" {...register("metersBuilt", { required: true })} className={styles.input} />
+      {errors.metersBuilt && <p className={styles.error}>This field is required</p>}
 
-      <input type="number" {...register("price", { required: true })} placeholder="Precio" />
-      {errors.price && <p>This field is required</p>}
+      <label htmlFor="price" className={styles.label}>Precio</label>
+      <input type="number" {...register("price", { required: true })} className={styles.input} />
+      {errors.price && <p className={styles.error}>This field is required</p>}
 
-      <input {...register("location", { required: true })} placeholder="Población" />
-      {errors.location && <p>This field is required</p>}
+      <label htmlFor="location" className={styles.label}>Población</label>
+      <input {...register("location", { required: true })} className={styles.input} />
+      {errors.location && <p className={styles.error}>This field is required</p>}
 
-      <select {...register("operation", { required: true })}>
+      <label htmlFor="operation" className={styles.label}>Operación</label>
+      <select {...register("operation", { required: true })} className={styles.select}>
         <option value="Vender">Vender</option>
         <option value="Alquiler">Alquiler</option>
         <option value="Compartir">Compartir</option>
       </select>
-      {errors.operation && <p>This field is required</p>}
+      {errors.operation && <p className={styles.error}>This field is required</p>}
 
-      <textarea {...register("description", { required: false })} placeholder="Descripción" />
-
-      <input type="submit" />
-    </form>
+      <label htmlFor="description" className={styles.label}>Descripción</label>
+      <textarea {...register("description", { required: false })} className={styles.textarea} />
+          
+          <input type="submit" className={styles.button} />
+        </form>
+      )}
+    </div>
   );
 };
 
