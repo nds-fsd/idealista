8ddc31924ff2d9edcd5762badd3189ebd5dbee12
Xavier Lopez <xlopezmin@gmail.com>
2024-01-10 12:59:32 +0100

    fix(users): update middleware response errors

--- backend/src/middleware/users.js
+++ backend/src/middleware/users.js
@@ -1,5 +1,6 @@
 const User = require("../mongo/schemas/users");
 
+
 const validateRegister = async (req, res, next) => {
     const { email, password, name } = req.body;
 
@@ -8,9 +9,8 @@ const validateRegister = async (req, res, next) => {
     }
 
     const validName = /^.{3,}$/
-
     if (!validName.test(name)) {
-        return res.status(400).json({ name: "El nombre debe tener al menos 3 caracteres" })
+        return res.status(400).json({ error: {name: "El nombre debe tener al menos 3 caracteres" }})
     }
 
     if (!email) {
@@ -18,24 +18,20 @@ const validateRegister = async (req, res, next) => {
     }
 
     const emailFormat = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
-
     if (!emailFormat.test(email)) {
-        return res.status(400).json({ email: "Este no es un email válido" })
+        return res.status(400).json({ error: { email: "Este no es un email válido" }})
     }
 
     if (!password) {
         return res.status(400).json({ error: { password: "Es requerida una contraseña" } });
     }
+
     const validatePassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/
-
     if (!validatePassword.test(password)) {
-        return res.status(400).json({ password: 'La contraseña debe tener al menos: 8 caracteres, una letra mayúscula, una letra minúscula y un número' })
-
-
+        return res.status(400).json({ error: { password: 'La contraseña debe tener al menos: 8 caracteres, una letra mayúscula, una letra minúscula y un número' }})
     }
 
     const user = await User.findOne({ email: email });
-
     if (user)
         return res.status(403).json({ error: { email: "Este email ya está registrado" } });
 
