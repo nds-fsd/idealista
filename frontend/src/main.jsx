623fcadff203b5a54e53ddef8641b71ff441bf4e
Filip Galetic <70514954+fgaletic@users.noreply.github.com>
2024-01-12 19:07:05 +0100

    Remove unused import statement

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from "react-query";
-import { Axios } from "axios";
 
 import App from "./App";
 import './index.css'
