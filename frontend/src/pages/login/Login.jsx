a5a726028f803d983043eca9475dda5ce145fbae
Harold <haroldtpozo@gmail.com>
2024-01-16 23:55:58 +0100

    Añadir funcionalidad de inicio de sesión y redirección con navigate. Añadir estilos al formulario

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -2,20 +2,26 @@ import React from "react";
 import {useForm} from "react-hook-form";
 import axios from "axios";
 import { loginUser } from "../../utils/apis/userApi";
+import {useNavigate} from "react-router-dom";
+
 import styles from "../../pages/login/Login.module.css"
+
 
 import login_foto from "../../assets/login_foto.jpg"
 
 
 const LoginForm = () =>{
-    const {register, handleSubmit,setError,formState:{errors}}= useForm();
+    const {register, handleSubmit,setError,reset,formState:{errors}}= useForm();
+    const navigate = useNavigate();
 
     const onSubmit = async(data)=>{
         try{
             const response = await loginUser(data);
             
             if (response.status === 200) {
-                console.log("Login existoso")
+                console.log("Login existoso");
+                alert("Login exitoso")
+                navigate("/")
             } else {
                 setError("apiError",{
                     type:"manual",
@@ -32,6 +38,7 @@ const LoginForm = () =>{
 return (
     <div className={styles.container}>
         <div className={styles.image_container}>
+            <div className={styles.text_login}><span>Login</span></div>
             <img className={styles.img} src={login_foto} alt="login imagen"/>
         </div>
 
@@ -49,7 +56,7 @@ return (
                     {errors.email && <div className={styles.error_message}>{errors.password.message}</div>}
                 </div>
 
-                <button className={styles.form_button} type="submit">Iniciar sesión</button>
+                <button className={styles.form_button} type="submit"><span>Iniciar sesión</span></button>
             </form>
         </div>
     </div>
