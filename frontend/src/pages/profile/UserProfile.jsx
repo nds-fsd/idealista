5fa349aae27bf3954230670b36f48c2ce8ed0d55
Filip Galetic <70514954+fgaletic@users.noreply.github.com>
2024-02-09 21:10:39 +0100

    Refactor UserProfile component to fetch user data using the user context

--- frontend/src/pages/profile/UserProfile.jsx
+++ frontend/src/pages/profile/UserProfile.jsx
@@ -7,15 +7,15 @@ import UserContext from "../../context/UserContext";
 const UserProfile = () => {
     const [userData, setUserData] = useState(null);
 
-    //const {user} = useContext(UserContext) // Esto tiene que funcionar
-    //console.log("User", user)
-    const id = '659e8ca8b95a7cb7353b6ddd';
+    const {user} = useContext(UserContext) // Esto tiene que funcionar
+    console.log("User", user)
+    // const id = '659e8ca8b95a7cb7353b6ddd';
 
     useEffect(() => {
         const fetchUserData = async () => {
             try {
-                const response = await getUser(id); // Replace with the actual API call to fetch user data
-                setUserData(response.data); // Assuming the response contains the user data
+                const response = await getUser(user._id); 
+                setUserData(response.data);
             } catch (error) {
                 console.error("Error fetching user data:", error);
             }
