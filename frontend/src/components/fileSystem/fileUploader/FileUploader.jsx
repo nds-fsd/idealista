c8105b8feee922d6dca32534bff8680092214088
Xavier Lopez <xlopezmin@gmail.com>
2024-02-10 21:10:46 +0100

    feac(realestateform): init upload image and look&feel

--- frontend/src/components/fileSystem/fileUploader/FileUploader.jsx
+++ frontend/src/components/fileSystem/fileUploader/FileUploader.jsx
@@ -1,26 +1,57 @@
 import React from "react";
 import { useState } from "react";
+import axios from "axios"
 
-import GooDriveApi from "../../../utils/apis/googleDriveApi";
 
 
 const FileUploader = () => {
     const [files, setFiles] = useState([]);
+    const [Loading, setLoading] = useState("");
 
     const handleDrop = (e) => {
         e.preventDefault();
         const newFiles = [...e.dataTransfer.files];
         setFiles((prevFiles) => [...prevFiles, ...newFiles]);
+        uploadFiles()
     }
 
     const handleFileChange = (e) => {
         console.log("event.target.files:", e.target.files)
         const newFiles = [...e.target.files];
         setFiles((prevFiles) => [...prevFiles, ...newFiles]);
+        uploadFiles(newFiles)
+    }
+
+    const uploadFiles = (ficheros) => {
+        console.log("uploadFiles:", ficheros)
+
+        const uploader = ficheros.map((file) => {
+            const formData = new FormData();
+            formData.append("file", file);
+            formData.append("tags", `codeinfuse, medium, gist`);
+            formData.append("upload_preset", "realista");
+            formData.append("api_key", "938244229631159");
+            formData.append("timestamp", (Date.now() / 1000 | 0));
+            setLoading("true");
+
+            return axios
+                    .post("https://api.cloudinary.com/dlrq6unnd/image/upload", formData, {
+                        headers: {"X-Requested-With": "XMLHttpRequest"}
+                    })
+                    .then((response) => {
+                        const data = response.data;
+                        const fileURL = data.secure_url;
+                        console.log("response.data", data, fileURL);
+                    })
+        })
+
+        axios.all(uploader).then(() => {
+            setLoading("false");
+        })
     }
 
     return (
-        <>    
+        <div style={{width: "97%"}}>    
             <div onDrop={handleDrop}
                 onDragOver={(e) => e.preventDefault()}
                 style={{ border: '1px dashed black', padding: '20px', marginBottom: '20px' }}>
@@ -41,10 +72,7 @@ const FileUploader = () => {
                 </ul>
             )}
             </div>
-            <div>
-                <button>Publicar</button>
-            </div>
-        </>
+        </div>
     )
 }
 
---
+++ frontend/src/utils/apis/claudinaryApi.js
@@ -0,0 +1,14 @@
+import {Cloudinary} from "@cloudinary/url-gen";
+
+const url = "https://api.cloudinary.com/v1_1/dlrq6unnd/"
+
+const uploadFiles = (files) => {
+    try {
+
+
+    } catch(error) {
+        console.log("Error in claudinaryApi.js:", error.message);
+    }
+}
+ 
+export default { uploadFiles }
