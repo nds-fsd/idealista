619b7aed1c643a285fc49a07094a3c38a98acf7f
Filip Galetic <70514954+fgaletic@users.noreply.github.com>
2024-02-13 23:41:20 +0100

    Refactor UserProfile component to extract PersonalData component

--- frontend/src/pages/profile/UserProfile/UserProfile.jsx
+++ frontend/src/pages/profile/UserProfile/UserProfile.jsx
@@ -1,61 +1,56 @@
 import { useEffect, useState, useContext } from "react";
-
 import styles from "./userProfile.module.css";
 import { getUser } from "../../../utils/apis/userApi";
 import UserContext from "../../../context/UserContext";
+import PersonalData from '../PersonalData/PersonalData';
 
 const UserProfile = () => {
-    const [userData, setUserData] = useState(null);
+  const [userData, setUserData] = useState(null);
 
-    const {user} = useContext(UserContext)
+  const { user } = useContext(UserContext);
 
-    useEffect(() => {
-        const fetchUserData = async () => {
-            try {
-                const response = await getUser(user._id); 
-                setUserData(response.data);
-            } catch (error) {
-                console.error("Error fetching user data:", error);
-            }
-        };
-        fetchUserData();
-    }, []);
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await getUser(user._id);
+        setUserData(response.data);
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    };
+    fetchUserData();
+  }, []);
 
-    return (
-        <div className={styles.userProfile}>
-        <>
+  return (
+    <div className={styles.userProfile}>
+      <>
         <h1>{userData?.name}</h1>
         <h2>{userData?.email}</h2>
-        </>
-        {/* Tab nav */}
-        <ul className={styles.tabnav}>
-            <li className={styles.navitemselected}>
-                <a className={styles.navlinkactive} href="#">Datos personales</a>
-            </li>
-            <li className={styles.navitem}>
-                <a className={styles.navlink} href="#">Mis publicaciones</a>
-            </li>
-            <li className={styles.navitem}>
-                <a className={styles.navlink} href="#">Mis favoritos</a>
-            </li>
-        </ul>
-        {/* Tab content */}
-        <div className={styles.tabcontent}>
-            {userData && (
-                <>
-                    <p>Nombre: {userData.name}</p>
-                    <p>Correo electronico: {userData.email}</p>
-                    <p>Ubicación: {userData.location}</p>
-                    <p>Calle: {userData?.street || ""}</p>
-                    <p>Número de Calle: {userData?.streetNumber || ""}</p>
-                    <p>Código Postal: {userData?.postalCode || ""}</p>
-                    <p>Provincia: {userData?.province || ""}</p>
-                    <p>Sobre mí: {userData?.aboutMe || ""}</p>      
-                </>
-            )}
-        </div>
-        </div>
-    );
-    }
+      </>
+      {/* Tab nav */}
+      <ul className={styles.tabnav}>
+        <li className={styles.navitemselected}>
+          <a className={styles.navlinkactive} href="#">
+            Datos personales
+          </a>
+        </li>
+        <li className={styles.navitem}>
+          <a className={styles.navlink} href="#">
+            Mis publicaciones
+          </a>
+        </li>
+        <li className={styles.navitem}>
+          <a className={styles.navlink} href="#">
+            Mis favoritos
+          </a>
+        </li>
+      </ul>
+      {/* Tab content */}
+      <div className={styles.tabcontent}>
+      <PersonalData userData={userData} />
+      </div>
+    </div>
+  );
+};
 
 export default UserProfile;
