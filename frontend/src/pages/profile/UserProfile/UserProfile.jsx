7c77c684e4921b8b5b7328ffcc6ae47b5f479c54
Filip Galetic <70514954+fgaletic@users.noreply.github.com>
2024-02-16 16:11:03 +0100

    Refactor UserProfile component

--- frontend/src/pages/profile/UserProfile/UserProfile.jsx
+++ frontend/src/pages/profile/UserProfile/UserProfile.jsx
@@ -2,94 +2,127 @@ import { useEffect, useState, useContext } from "react";
 import styles from "./userProfile.module.css";
 import { getUser, updateUser } from "../../../utils/apis/userApi";
 import UserContext from "../../../context/UserContext";
-import PersonalData from '../PersonalData/PersonalData';
-import MyAds from '../MyAds/MyAds';
-import MyFavorites from '../MyFavorites/MyFavorites';
+import PersonalData from "../PersonalData/PersonalData";
+import MyAds from "../MyAds/MyAds";
+import MyFavorites from "../MyFavorites/MyFavorites";
 
 const UserProfile = () => {
-    const [userData, setUserData] = useState(null);
-    const [activeTab, setActiveTab] = useState("personalData");
+  const [userData, setUserData] = useState(null);
+  const [activeTab, setActiveTab] = useState("personalData");
 
-    const { user } = useContext(UserContext);
+  const { user } = useContext(UserContext);
 
-    useEffect(() => {
-        const fetchUserData = async () => {
-            try {
-                const response = await getUser(user._id);
-                setUserData(response.data);
-            } catch (error) {
-                console.error("Error fetching user data:", error);
-            }
-        };
-        fetchUserData();
-    }, []);
-
-    const handleTabClick = (tab) => {
-        setActiveTab(tab);
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await getUser(user._id);
+        setUserData(response.data);
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
     };
+    fetchUserData();
+  }, []);
 
-    const handleUpdateUser = async (data) => {
-        console.log("ESTA ES LA DATA DEL USUARIO0", data)
-        try {
-            const response = await updateUser(user._id, data);
-            if (response) {
-                setUserData(response.data);
-            } else {
-                console.error("No response from updateUser");
+  const handleTabClick = (tab) => {
+    setActiveTab(tab);
+  };
+
+  const handleUpdateUser = async (data) => {
+    try {
+      const response = await updateUser(user._id, data);
+      if (response) {
+        setUserData(response.data);
+      } else {
+        console.error("No response from updateUser");
+      }
+    } catch (error) {
+      console.error("Error updating user data:", error);
+    }
+  };
+
+  return (
+    <div className={styles.userProfile}>
+      <>
+        <h1>{userData?.name}</h1>
+        <h2>{userData?.email}</h2>
+      </>
+      {/* Tab nav */}
+      <ul className={styles.tabnav}>
+        <li
+          className={
+            activeTab === "personalData"
+              ? styles.navitemselected
+              : styles.navitem
+          }
+        >
+          <a
+            className={
+              activeTab === "personalData"
+                ? styles.navlinkactive
+                : styles.navlink
             }
-        } catch (error) {
-            console.error("Error updating user data:", error);
-        }
-    };
-
-    return (
-        <div className={styles.userProfile}>
-            <>
-                <h1>{userData?.name}</h1>
-                <h2>{userData?.email}</h2>
-            </>
-            {/* Tab nav */}
-            <ul className={styles.tabnav}>
-                <li className={activeTab === "personalData" ? styles.navitemselected : styles.navitem}>
-                    <a
-                        className={activeTab === "personalData" ? styles.navlinkactive : styles.navlink}
-                        href="#"
-                        onClick={() => handleTabClick("personalData")}
-                    >
-                        Datos personales
-                    </a>
-                </li>
-                <li className={activeTab === "publications" ? styles.navitemselected : styles.navitem}>
-                    <a
-                        className={activeTab === "publications" ? styles.navlinkactive : styles.navlink}
-                        href="#"
-                        onClick={() => handleTabClick("publications")}
-                    >
-                        Mis publicaciones
-                    </a>
-                </li>
-                <li className={activeTab === "favorites" ? styles.navitemselected : styles.navitem}>
-                    <a
-                        className={activeTab === "favorites" ? styles.navlinkactive : styles.navlink}
-                        href="#"
-                        onClick={() => handleTabClick("favorites")}
-                    >
-                        Mis favoritos
-                    </a>
-                </li>
-            </ul>
-            {/* Tab content */}
-            <div className={styles.tabcontent}>
-                {activeTab === "personalData" && <PersonalData userData={userData} handleUpdateUser={handleUpdateUser} />}
-                {activeTab === "publications" && (
-                    <MyAds ads={userData?.ads} emptyMessage="No tienes ningún anuncio" />
-                )}
-                {activeTab === "favorites" && (
-                    <MyFavorites favorites={userData?.favorites} emptyMessage="No tienes ningún favorito" />
-                )}
-            </div>
-        </div>
-    );
+            href="#"
+            onClick={() => handleTabClick("personalData")}
+          >
+            Datos personales
+          </a>
+        </li>
+        <li
+          className={
+            activeTab === "publications"
+              ? styles.navitemselected
+              : styles.navitem
+          }
+        >
+          <a
+            className={
+              activeTab === "publications"
+                ? styles.navlinkactive
+                : styles.navlink
+            }
+            href="#"
+            onClick={() => handleTabClick("publications")}
+          >
+            Mis publicaciones
+          </a>
+        </li>
+        <li
+          className={
+            activeTab === "favorites" ? styles.navitemselected : styles.navitem
+          }
+        >
+          <a
+            className={
+              activeTab === "favorites" ? styles.navlinkactive : styles.navlink
+            }
+            href="#"
+            onClick={() => handleTabClick("favorites")}
+          >
+            Mis favoritos
+          </a>
+        </li>
+      </ul>
+      {/* Tab content */}
+      <div className={styles.tabcontent}>
+        {activeTab === "personalData" && (
+          <PersonalData
+            userData={userData}
+            handleUpdateUser={handleUpdateUser}
+          />
+        )}
+        {activeTab === "publications" && (
+          <MyAds ads={userData?.ads} emptyMessage="No tienes ningún anuncio" />
+        )}
+        {activeTab === "favorites" && (
+          <MyFavorites
+            favorites={userData?.favorites}
+            emptyMessage="No tienes ningún favorito"
+          />
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default UserProfile;
