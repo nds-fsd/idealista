74c07fc31bc424c97d383ea4de3d6d96eb4fa282
Xavier Lopez <xlopezmin@gmail.com>
2024-02-20 23:17:45 +0100

    feac(realestatedetail): display remote real estate images

--- frontend/src/pages/realEstates/realEstateDetails/RealEstateDetailsCarousel.jsx
+++ frontend/src/pages/realEstates/realEstateDetails/RealEstateDetailsCarousel.jsx
@@ -1,26 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Slider from "react-slick";
 
 import styles from "./RealEstateDetailsCarousel.module.css";
 import house_image from "../../../assets/pexels-aaron-cook-19277901 1.png"
 
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-const Carousel = () => {
-    const images = [house_image, house_image, house_image]
+
+const Carousel = (props) => {
+    const {height, width, images} = props || []
+    if (images.length === 0) images.push = house_image;
+
+/*    useEffect(() => {
+    }, [images])
+*/
+
     const settings = {
         useCSS: true,
         dots: true,
-        speed: 400,
+        speed: 500,
         slidesToShow: 1,
         slidesToScroll: 1,
         infinite: true,
         autoplay: true,
-        autoplaySpeed: 4000,
+        autoplaySpeed: 5000
     };
 
     return (
         <Slider {...settings}>
-            {images.map((i, index) => <img key={`image_carousel_${index}`} className={styles.rounded} src={i} />)}
+            {images.map((i, index) =>   <div style={{height:props.height, width:props.width}} key={"div"+i}>
+                                            <img key={`image_carousel_${index}`} 
+                                                 className={styles.rounded} 
+                                                 src={i}
+                                                 style={{height:props.height, width:"auto", margin:"auto"}} />
+                                        </div>)}
         </Slider>
     )
 };
