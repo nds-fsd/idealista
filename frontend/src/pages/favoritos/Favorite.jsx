68c764b840a2c5e7b500e5f675fe12bc4188e6ba
Harold <haroldtpozo@gmail.com>
2024-02-23 20:11:09 +0100

    Mejoras en Favoritos

--- frontend/src/pages/favoritos/Favorite.jsx
+++ frontend/src/pages/favoritos/Favorite.jsx
@@ -1,35 +1,85 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import favoriteApi from "../../utils/apis/favoriteApi";
 import RealEstateListElement from "../realEstates/realEstateList/RealEstateListElement";
 import { useQuery } from "react-query";
 import UserContext from "../../context/UserContext";
-import styles from "./Favorite.module.css"
+import styles from "./Favorite.module.css";
+import imageFavoritos from "../../assets/Favoritos.jpg";
+import CircularProgress from "@mui/material/CircularProgress";
 
 const Favorite = () => {
-    const { user } = useContext(UserContext)
-    const { data, isLoading, refetch } = useQuery(['favorite', user], () => favoriteApi.getFavorites({ userId: user._id }), { enabled: Boolean(user?._id) });
-
-    if (isLoading) return <div>Is Loading...</div>;
-
-    if (!data) return <div> Something went wrong</div>;
-
-    const deleteById = async (realEstate) => {
-        await favoriteApi.removeById({ userId: user._id, realestateId: realEstate._id })
-        refetch()
+  const { user } = useContext(UserContext);
+  const { data, isLoading, refetch } = useQuery(["favorite", user], () => {
+    if (user && user._id) {
+      return favoriteApi.getFavorites({ userId: user._id });
+    } else {
+      return Promise.resolve(null);
     }
+  });
 
+  useEffect(() => {
+    refetch();
+  }, [user, refetch]);
+
+  const deleteById = async (realEstate) => {
+    await favoriteApi.removeById({ userId: user._id, realestateId: realEstate._id });
+    refetch();
+  };
+
+  if (isLoading) {
     return (
-        <div style={{ marginLeft: "175px" }}>
-            {data.map(e => {
-                const realEstate = e.realEstate;
-                realEstate.fav = true;
-                return <RealEstateListElement key={realEstate._id} realEstate={realEstate} onFavorite={deleteById} />
-            })}
+      <div className={styles.containerloading}>
+        <CircularProgress />
+      </div>
+    );
+  }
+
+  if (user && data && data.length > 0) {
+    return (
+      <div className={styles.container}>
+        <div className={styles.header}>
+          <div className={styles.imghead}>
+            <img src={imageFavoritos} alt="imagen Mis Favoritos" />
+            <div className={styles.text_misanuncios}>
+              <span>Mis favoritos</span>
+            </div>
+          </div>
         </div>
-
-    )
-
-}
-
+        <div className={styles.listfavoritos}>
+          <div className={styles.favoritosContainer}>
+            {data.map((e) => {
+              const realEstate = e.realEstate;
+              realEstate.fav = true;
+              return <RealEstateListElement key={realEstate._id} realEstate={realEstate} onFavorite={deleteById} />;
+            })}
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className={styles.container}>
+        <div className={styles.header}>
+          <div className={styles.imghead}>
+            <img src={imageFavoritos} alt="imagen Mis Favoritos" />
+            <div className={styles.text_misanuncios}>
+              <h1>Mis favoritos</h1>
+            </div>
+          </div>
+        </div>
+        <div className={styles.listrealestates}>
+          <div className={styles.nodata}>
+            <h2>¡Hazlo real en Realista!</h2>
+            <p>
+              <span>{user.name}</span> tus favoritos están vacíos.
+            </p>
+            <div style={{ height: "20px" }}></div>
+            <span> Explora nuestra selección y marca tus propiedades favoritas con un simple clic</span>
+          </div>
+        </div>
+      </div>
+    );
+  }
+};
 
 export default Favorite;
