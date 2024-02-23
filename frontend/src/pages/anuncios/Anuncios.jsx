4139228860c1a1e9e5a90822210cb75aa6f07278
Harold <haroldtpozo@gmail.com>
2024-02-23 13:00:57 +0100

    Ya se puede ver los anuncios que ha creado el usuario

--- frontend/src/pages/anuncios/Anuncios.jsx
+++ frontend/src/pages/anuncios/Anuncios.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import realEstateApi from "../../utils/apis/realEstateApi";
 import RealEstateListElement from "../realEstates/realEstateList/RealEstateListElement";
 import { useQuery } from "react-query";
@@ -7,12 +7,29 @@ import styles from "./Anuncios.module.css";
 
 const Anuncios = () => {
   const { user } = useContext(UserContext);
-  const { data, isLoading, refetch } = useQuery(["realestates", user], () => realEstateApi.GetByUserId({ userId: user._id }));
+  const { data, isLoading, isError, refetch } = useQuery(["realestates", user ? user._id : null], () => {
+    if (user) {
+      return realEstateApi.GetByUserId(user._id);
+    } else {
+      return Promise.resolve(null);
+    }
+  });
+
+  useEffect(() => {
+    refetch();
+  }, [user, refetch]);
 
   if (isLoading) return <div> Is Loading...</div>;
-  if (!data) return <div>Something went wrong</div>;
+  if (isError) return <div>Something went wrong</div>;
+  if (!data) return <div>No data available</div>;
 
-  return <div>Anuncios</div>;
+  return (
+    <div className={styles.anunciosContainer}>
+      {data.map((realEstate) => (
+        <RealEstateListElement key={realEstate._id} realEstate={realEstate} />
+      ))}
+    </div>
+  );
 };
 
 export default Anuncios;
