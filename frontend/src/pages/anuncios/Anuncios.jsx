81e5e0c4d7e335b4fa993653d8cbc76ce1e538e9
Harold <haroldtpozo@gmail.com>
2024-02-23 17:45:21 +0100

    Se puede consultar los anuncios creados por el ususario. Si no hay data hay un texto que invita al usuario a publicar un anuncio. Hay mejoras en el css

--- frontend/src/pages/anuncios/Anuncios.jsx
+++ frontend/src/pages/anuncios/Anuncios.jsx
@@ -1,9 +1,12 @@
 import React, { useContext, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import realEstateApi from "../../utils/apis/realEstateApi";
 import RealEstateListElement from "../realEstates/realEstateList/RealEstateListElement";
 import { useQuery } from "react-query";
 import UserContext from "../../context/UserContext";
 import styles from "./Anuncios.module.css";
+import imageAnuncio from "../../assets/Mis_anuncios.jpg";
+import CircularProgress from "@mui/material/CircularProgress";
 
 const Anuncios = () => {
   const { user } = useContext(UserContext);
@@ -19,17 +22,61 @@ const Anuncios = () => {
     refetch();
   }, [user, refetch]);
 
-  if (isLoading) return <div> Is Loading...</div>;
-  if (isError) return <div>Something went wrong</div>;
-  if (!data) return <div>No data available</div>;
+  if (isLoading) {
+    return (
+      <div className={styles.containerloading}>
+        <CircularProgress />
+      </div>
+    );
+  }
 
-  return (
-    <div className={styles.anunciosContainer}>
-      {data.map((realEstate) => (
-        <RealEstateListElement key={realEstate._id} realEstate={realEstate} />
-      ))}
-    </div>
-  );
+  if (isError) return <div>Something went wrong</div>;
+
+  if (user && data && data.length > 0) {
+    return (
+      <div className={styles.container}>
+        <div className={styles.header}>
+          <div className={styles.imghead}>
+            <img src={imageAnuncio} alt="imagen Mis anuncios" />
+            <div className={styles.text_misanuncios}>
+              <span>Mis anuncios</span>
+            </div>
+          </div>
+        </div>
+        <div className={styles.listrealestates}>
+          <div className={styles.anunciosContainer}>
+            {data.map((realEstate) => (
+              <RealEstateListElement key={realEstate._id} realEstate={realEstate} />
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className={styles.container}>
+        <div className={styles.header}>
+          <div className={styles.imghead}>
+            <img src={imageAnuncio} alt="imagen Mis anuncios" />
+            <div className={styles.text_misanuncios}>
+              <h1>Mis anuncios</h1>
+            </div>
+          </div>
+        </div>
+        <div className={styles.listrealestates}>
+          <div className={styles.nodata}>
+            <h2>¡Hazlo real en Realista!</h2>
+            <p>Aún no has publicado ningún anuncio.</p>
+            <div style={{ height: "20px" }}></div>
+            <span>¡Es hora de mostrar tus propiedades al mundo!</span>
+            <Link to="/realestates/create" className={styles.navcontent}>
+              <button className={styles.buttonad}>Publica gratis tu anuncio</button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    );
+  }
 };
 
 export default Anuncios;
