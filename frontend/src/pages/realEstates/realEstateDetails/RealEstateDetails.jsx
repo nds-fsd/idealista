d7e3ffb170294b175c8658fc529890ed5843055b
Harold <haroldtpozo@gmail.com>
2024-02-25 00:21:55 +0100

    Mejora en los detalles de inmuebles

--- frontend/src/pages/realEstates/realEstateDetails/RealEstateDetails.jsx
+++ frontend/src/pages/realEstates/realEstateDetails/RealEstateDetails.jsx
@@ -9,94 +9,96 @@ import Carousel from "./RealEstateDetailsCarousel";
 import GoogleMapsReactWrapper from "../../../components/googleMaps/reactWrapper/GoogleMapsReactWrapper";
 import GoogleMapsIndividual from "../../../components/googleMaps/map/GoogleMapsIndividual";
 
-import styles from "./RealEstateDetails.module.css"
-import compartir from "../../../assets/compartir.png"
-import likeImag from "../../../assets/me-gusta.png"
-import TextArea from "./TextArea"
+import styles from "./RealEstateDetails.module.css";
+import compartir from "../../../assets/compartir.png";
+import likeImag from "../../../assets/me-gusta.png";
+import TextArea from "./TextArea";
 
 import UseAnimation from "react-useanimations";
 import heart from "react-useanimations/lib/heart";
 import favoriteApi from "../../../utils/apis/favoriteApi";
 
 const RealEstateDetails = () => {
-    const { id } = useParams();
-    const { user } = useContext(UserContext);
-    const { data, isLoading } = useQuery('realEstateDetail', () => realEstateApi.GetRealEstate(id));
+  const { id } = useParams();
+  const { user } = useContext(UserContext);
+  const { data, isLoading } = useQuery("realEstateDetail", () => realEstateApi.GetRealEstate(id));
 
-    const setFavorite = async ({ _id }) => {
-        favoriteApi.addFavorite({ userId: user._id, realestateId: _id })
-    }
-    useEffect(() => {
-        window.scrollTo(0, 0);
-    }, [])
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
+  const setFavorite = async ({ _id }) => {
+    favoriteApi.addFavorite({ userId: user._id, realestateId: _id });
+  };
 
+  if (isLoading) return <div>Is Loading...</div>;
+  if (!data) return <div> Something went wrong</div>;
 
-    if (isLoading) return <div>Is Loading...</div>;
-    if (!data) return <div> Something went wrong</div>;
-
-    return (
-        <div style={{ width: "1140px", margin: "auto" }}>
-            <div className={styles.carousel_container}>
-                <Carousel height={"500px"} width={"1140px"} images={data.images} />
+  return (
+    <div style={{ width: "1300px", margin: "auto" }}>
+      <div className={styles.carousel_container}>
+        <Carousel height={"500px"} width={"1140px"} images={data.images} />
+      </div>
+      <div className={styles.container}>
+        <div className={styles.container1}>
+          <div style={{ display: "flex", height: "120px", justifyContent: "space-between" }}>
+            <div style={{ display: "flex", alignItems: "center", marginLeft: "30px" }}>
+              <UseAnimation animation={heart} reverse={data.fav} fillColor="#CFE2FF" size={35} onClick={() => setFavorite(data)} />
+              <span style={{}}>Me gusta</span>
             </div>
-
-            <div className={styles.buttons}>
-                <div style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
-                    <UseAnimation animation={heart} reverse={data.fav} fillColor="#CFE2FF" size={35} onClick={() => setFavorite(data)} />
-                    <span style={{ marginLeft: "5px" }}>Me gusta</span>
+            <div style={{ display: "flex", alignItems: "center", marginRight: "30px" }}>
+              <h4 style={{ color: "#6D96FF" }}>{data?.price} €</h4>
+            </div>
+          </div>
+          <div style={{ height: "100%" }}>
+            <div className={styles.title}>
+              <p>
+                {data?.roadName}, {data?.location}
+              </p>
+              <h1>{data?.shortDescription}</h1>
+            </div>
+            <div style={{ margin: "10px 30px 10px 30px" }}>
+              <p style={{ textAlign: "justify", lineHeight: "1.8" }}>{data?.description}</p>
+            </div>
+            <div style={{ margin: "30px 30px 30px 30px" }}>
+              <div className={styles.caracteristicas}>
+                <h2>Características básicas</h2>
+                <div className={styles.columnlist}>
+                  <p style={{ paddingRight: "24px", borderRight: "1px solid", fontWeight: "700" }}>{data?.realEstateType + ": " + data?.realEstateSubtype}</p>
+                  <p style={{ paddingLeft: "24px", paddingRight: "24px", borderRight: "1px solid", fontWeight: "700" }}>{data?.metersBuilt + " m2"}</p>
+                  <p style={{ paddingLeft: "24px", paddingRight: "24px", fontWeight: "700" }}>{data?.state}</p>
                 </div>
-                <h4 style={{ color: "#6D96FF" }} >
-                    {data?.price} €
-                </h4>
-                <TextArea toUserId={data?.user}> </TextArea>
-            </div>
-
-            <div className={styles.container_text}>
-                <h3> {data?.shortDescription} </h3>
-                <h3> {data?.location}</h3>
-                <textarea style={{
-                    fontSize: "16px", border: "none", outline: "none",
-                    minHeight: "550px",
-                    minWidth: "1140px", maxWidth: "1140px"
-                }}
-                    readOnly
-                    value={data?.description} />
-            </div>
-
-            <div className={styles.columnContainer}>
-                <div className={styles.leftColumn}>
-                    <div className={styles.caracteristicas}>
-                        <h2>Características básicas</h2>
-                        <div>
-                            <p>{data?.realEstateType + ": " + data?.realEstateSubtype}</p>
-                            <p>{data?.properties.map((element) => {
-                                return <span key={element}>{element + " "}</span>
-                            })}</p>
-                            <p>{data?.metersBuilt + " m2"}</p>
-                            <p>{data?.state}</p>
-                            <br></br>
-
-                        </div>
-                    </div>
+                <div style={{ display: "flex", gap: "10px", marginTop: "20px" }}>
+                  <p style={{ display: "flex", flexDirection: "column" }}>
+                    {data?.properties.map((element) => {
+                      return <span key={element}>{element + " "}</span>;
+                    })}
+                  </p>
                 </div>
-                <div className={styles.rightColumn}>
-                    <GoogleMapsReactWrapper>
-                        <GoogleMapsIndividual
-                            center={{
-                                lat: data?.publicMapLocation?.coordinates[0],
-                                lng: data?.publicMapLocation?.coordinates[1]
-                            }}
-                            zoom={15}
-                            style={{ margin: "auto", width: "300px", height: "300px" }}
-                        >
-                        </GoogleMapsIndividual>
-                    </GoogleMapsReactWrapper>
-
-                </div>
+              </div>
             </div>
+          </div>
         </div>
-    )
-}
+        <div className={styles.container2}>
+          <div style={{ display: "flex", height: "120px", justifyContent: "center" }}>
+            <TextArea toUserId={data?.user}> </TextArea>
+          </div>
+          <div className={styles.rightColumn}>
+            <GoogleMapsReactWrapper style={{ borderRadius: "20px" }}>
+              <GoogleMapsIndividual
+                center={{
+                  lat: data?.publicMapLocation?.coordinates[0],
+                  lng: data?.publicMapLocation?.coordinates[1],
+                }}
+                zoom={15}
+                style={{ width: "300px", height: "300px" }}
+              ></GoogleMapsIndividual>
+            </GoogleMapsReactWrapper>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default RealEstateDetails;
