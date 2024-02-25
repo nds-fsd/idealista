6f267aec9ec627409a49f7c6862bee66f496d44f
Filip Galetic <70514954+fgaletic@users.noreply.github.com>
2024-02-25 18:35:11 +0100

    Update link to view all ads

--- frontend/src/pages/realEstates/realEstateForm/realEstateForm.jsx
+++ frontend/src/pages/realEstates/realEstateForm/realEstateForm.jsx
@@ -37,7 +37,7 @@ const RealEstateForm = () => {
         <div className={styles.container}>
           <p className={styles.successMessage}>¡Tu anuncio ha sido publicado exitosamente!</p>
           <a href="/" className={styles.link}>Volver a la página de inicio</a>
-          <a href="/realestates/create" className={styles.link}>Crear otro anuncio</a>
+            <a href="/anuncios" className={styles.link}>Ver todos tus anuncios</a>
         </div>
       ) : isSuccessful === false ? (
         <div className={styles.container}>
